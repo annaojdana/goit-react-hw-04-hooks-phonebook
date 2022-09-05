ac99bd8eed5c5a35b6de28d37a09923362680c7c
Anna Ojdana <anna.ojdana@gmail.com>
2022-09-05 10:00:34 +0200

    filter - code refactor

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,20 +1,22 @@
 import styles from './Filter.module.css';
 import React from 'react';
 import PropTypes from 'prop-types';
-class Filter extends React.Component {
-  render() {
-    const { onChange } = this.props;
-    const { field, text, input } = styles;
-    return (
-      <div className={field}>
+
+
+const Filter = ({ onChange }) => {
+  const { field, text, input } = styles;
+
+  return (
+   <div className={field}>
         <p className={text}>Find contact by name</p>
         <input className={input} type="text" onChange={onChange} />
       </div>
-    );
-  }
-}
+  );
+};
+
 
 export default Filter;
+
 Filter.propTypes = {
   onChange: PropTypes.func
 };
